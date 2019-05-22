d96cf714596a25746cf469cf86df312db2e35dd1
Aleksandr <zatursky.lex@gmail.com>
2019-05-22 10:57:49 +0300

    aaa

--- DBInit.js
+++ DBInit.js
@@ -71,8 +71,7 @@ async function initializeUser(connection) {
     const gender = firstName === 'Anna' ? 'female' : 'male';
     const SL = `https://www.instagram.com/${firstName.toLowerCase()}_${lastName.toLowerCase()}`;
     const roleId = i ? roles[1].id : roles[0].id;
-    const user = new User(
-      firstName,
+    const user = new User(sss
       lastName,
       email,
       password,
