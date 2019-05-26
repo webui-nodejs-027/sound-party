51f346568a87bd050d948c71764f9574ae149790
Vadim <vadim.feoklistov12@gmail.com>
2019-05-26 21:04:08 +0300

    add author crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const userRoute = require('./userRoute');
 const genreRoute = require('./genreRoute');
 const songRoute = require('./songRoute');
 const meetingRoute = require('./meetingRoute');
+const authorRoute = require('./authorRoute');
 
 module.exports = (app) => {
   app.use('/', mainRoute);
@@ -10,4 +11,5 @@ module.exports = (app) => {
   app.use('/api/genres', genreRoute);
   app.use('/api/songs', songRoute);
   app.use('/api/meetings', meetingRoute);
+  app.use('/api/authors', authorRoute);
 };
