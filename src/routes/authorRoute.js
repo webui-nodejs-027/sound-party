db6bd95c5dba3392365b78b6689954f3d5d74ac6
Unknown <vadim.feoklistov12@gmail.com>
2019-06-07 14:15:01 +0300

    add errorWrap on all genre & author routes

--- src/routes/authorRoute.js
+++ src/routes/authorRoute.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const authorController = require('../controllers/AuthorController');
 const { baseValidator, authorValidator } = require('../validators/index.js');
+const errorWrap = require('../middlewares/appMiddlewares/errorWrap');
 
 const router = express.Router();
 
-router.use('/:id', baseValidator.checkId);
+router.all('/:id', errorWrap(baseValidator.checkId));
 
-router.get('/:id', authorController.getById.bind(authorController));
-router.post(
-  '/',
-  authorValidator.checkBody,
-  authorController.insertData.bind(authorController),
-);
-router.put('/:id', authorController.updateById.bind(authorController));
-router.delete('/:id', authorController.deleteById.bind(authorController));
+router.get('/:id', errorWrap(authorController.getById.bind(authorController)));
+router.post('/', errorWrap(authorValidator.checkBody, authorController.insertData.bind(authorController)));
+router.put('/:id', errorWrap(authorController.updateById.bind(authorController)));
+router.delete('/:id', errorWrap(authorController.deleteById.bind(authorController)));
 
-router.get('/', authorController.getAllData.bind(authorController));
+router.get('/', errorWrap(authorController.getAllData.bind(authorController)));
 
 module.exports = router;
