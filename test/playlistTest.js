cb52da1b92ca97252eb46f9bbdf28c6daf1bc994
Nadia326 <nadiia.kovalova@nure.ua>
2019-07-01 22:39:41 +0300

    add tests for playlist

--- test/playlistTest.js
+++ test/playlistTest.js
@@ -2,23 +2,46 @@
 /* eslint-disable no-unused-expressions */
 process.env.NODE_ENV = 'test';
 
-const { describe, it } = require('mocha');
+const {
+ describe, it, beforeEach, before,
+} = require('mocha');
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const { container } = require('../src/ioc');
+const { TYPES } = require('../src/constants');
+
+chai.should();
+const { initialServer } = require('../src/app');
 
 chai.use(chaiHttp);
 
 const url = 'http://localhost:3001/api';
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInJvbGVJZCI6MSwiaWF0IjoxNTYxOTcwMzM1LCJleHAiOjE1NjIwNTY3MzV9.lK1jQhZD0s0OJo8zlk81Xkcj_zmGdPAfADfIpfuUGhk';
-let playlist = null;
+let playlistService = null;
+const playlistPost = {
+    name: "Peter's playlist",
+    favourite: false,
+    isMain: false,
+    userId: 1,
+};
 
 describe('Playlists', () => {
+  before(async () => {
+    await initialServer();
+  });
+
+  beforeEach(async () => {
+    playlistService = container.get(TYPES.PlaylistService);
+    await playlistService.insertData(playlistPost);
+  });
+
   it('should list ALL playlists  of user on /playlists GET', () => chai.request(url)
     .get('/playlists')
     .set('Authorization', token)
     .then((res) => {
-      res.should.have.status(200);
-      res.should.be.json;
+      res.status.should.equal(200);
+      res.type.should.equal('application/json');
       res.body.should.be.a('array');
       res.body[0].should.have.property('id');
       res.body[0].should.have.property('name');
@@ -37,8 +60,8 @@ describe('Playlists', () => {
         userId: 1,
       })
       .then((res) => {
-        res.should.have.status(201);
-        res.should.be.json;
+        res.status.should.equal(201);
+        res.type.should.equal('application/json');
         res.body.should.be.a('object');
         res.body.should.have.property('userId');
         res.body.should.have.property('name');
@@ -49,18 +72,17 @@ describe('Playlists', () => {
         res.body.favourite.should.equal(false);
         res.body.isMain.should.equal(false);
         res.body.userId.should.equal(1);
-        playlist = res.body;
       })
       .catch((e) => {
         throw e;
       }));
 
   it('should update a SINGLE playlist on /playlists/<id> PUT', () => chai.request(url)
-      .put(`/playlists/${playlist.id}`)
+      .put('/playlists/1')
       .send({ name: 'Spider' })
       .then((res) => {
-        res.should.have.status(200);
-        res.should.be.json;
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
         res.body.should.be.a('object');
         res.body.should.have.property('userId');
         res.body.should.have.property('name');
@@ -74,10 +96,10 @@ describe('Playlists', () => {
       }));
 
   it('should list ALL songs on /playlists/<id>/songs GET', () => chai.request(url)
-      .get(`/playlists/${playlist.id}/songs`)
+      .get('/playlists/1/songs')
       .then((res) => {
-        res.should.have.status(200);
-        res.should.be.json;
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
         res.body.should.be.a('array');
         res.body[0].should.have.property('id');
         res.body[0].should.have.property('name');
@@ -91,9 +113,10 @@ describe('Playlists', () => {
       }));
 
   it('should delete a SINGLE playlists on /playlists/<id> DELETE', () => chai.request(url)
-      .delete(`/playlists/${playlist.id}`)
+      .delete('/playlists/1')
       .then((res) => {
-        res.should.have.status(200);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
         res.body.should.be.a('number');
       })
       .catch((e) => {
